576f8c9c2d9c7633f70bcbe9c087491325e1efc4
PhillipKinney <phillipjkinney@gmail.com>
2020-09-23 12:29:34 -0400

    front end nav bar edits

--- src/main/java/com/dollardeveloper/employeemanagement/employeemanagement/web/MainController.java
+++ src/main/java/com/dollardeveloper/employeemanagement/employeemanagement/web/MainController.java
@@ -11,4 +11,10 @@ public class MainController {
     public String login(){
         return "login";
     }
+
+
+//    @GetMapping("/")
+//    public String home(){
+//        return "index";
+//    }
 }
