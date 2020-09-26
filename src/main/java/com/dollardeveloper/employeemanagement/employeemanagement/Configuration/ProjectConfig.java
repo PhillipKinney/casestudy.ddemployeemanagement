3caae2334e02512978eaf5818df28a1a42c61741
PhillipKinney <phillipjkinney@gmail.com>
2020-09-25 23:07:05 -0400

    adding dummy data since app is deployed and no longer connected to local database

---
+++ src/main/java/com/dollardeveloper/employeemanagement/employeemanagement/Configuration/ProjectConfig.java
@@ -0,0 +1,38 @@
+package com.dollardeveloper.employeemanagement.employeemanagement.Configuration;
+
+import com.dollardeveloper.employeemanagement.employeemanagement.model.Department;
+import com.dollardeveloper.employeemanagement.employeemanagement.model.Employee;
+import com.dollardeveloper.employeemanagement.employeemanagement.model.Post;
+import com.dollardeveloper.employeemanagement.employeemanagement.repository.DepartmentRepository;
+import com.dollardeveloper.employeemanagement.employeemanagement.repository.EmployeeRepository;
+import com.dollardeveloper.employeemanagement.employeemanagement.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+//I decided to add dummy data here as opposed to connecting a virutal database after I deployed to heroku.
+//Prior to this, I was not using this class, and made use of my local MySQL database.
+@Configuration
+public class ProjectConfig {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostConstruct
+    public void setup(){
+        employeeRepository.saveAll(Arrays.asList(
+                new Employee(),
+                new Employee(),
+                new Employee()
+        ));
+
+        postRepository.saveAll(Arrays.asList(
+                new Post(),
+                new Post(),
+                new Post()
+        ));
+    }
+}
